996115a45a51abf29b78baafd495dac80fa67603
Sachin Singh <sachinsingh9272@gmail.com>
2024-12-15 20:02:12 +0530

    Create a test api route

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+export const test = (req,res)=>{
+    res.json({message:'API is working'})
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import connectDB from './config/db.js';
+import userRoutes from './routes/user.route.js';
 const app = express();
 connectDB();
+
+// Routes 
+app.use('/api/user', userRoutes);
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     
