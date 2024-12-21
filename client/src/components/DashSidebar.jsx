15e85d2ec929c19525e30118773ea502cef54ccd
Sachin Singh <sachinsingh9272@gmail.com>
2024-12-21 12:14:35 +0530

    Complete Sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const DashSidebar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlparams = new URLSearchParams(location.search);
+    const tabFromUrl = urlparams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSidebar;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlparams = new URLSearchParams(location.search);
+    const tabFromUrl = urlparams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* Profile */}
+      {tab === "profile" && <DashProfile />}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
