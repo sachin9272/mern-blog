e496253de4f6224cd6e869819bc0b665f6aae76f
Sachin Singh <sachinsingh9272@gmail.com>
2024-12-21 11:27:27 +0530

    Complete Dark Mode Functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,12 @@ import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
+import themeReducer from './theme/themeSlice'
 
 
 const rootReducer = combineReducers({
     user: userReducer,
+    theme: themeReducer,
 });
 
 const persistConfig = {
