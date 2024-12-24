e1cc1141d41ae0d0603f9bdc5570ed55b6169d64
Sachin Singh <sachinsingh9272@gmail.com>
2024-12-24 22:34:42 +0530

    Add Update user api routes

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 connectDB();
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
  
 // Routes  
