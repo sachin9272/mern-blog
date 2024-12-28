14ab7840983ef3d480919cc4ccf8a380710859fa
Sachin Singh <sachinsingh9272@gmail.com>
2024-12-29 02:19:26 +0530

    show user posts inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,6 @@
 import flowbite from 'flowbite-react/tailwind';
 
 /** @type {import('tailwindcss').Config} */
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -12,5 +11,6 @@ module.exports = {
   },
   plugins: [
     flowbite.plugin(),
+    require('flowbite/plugin')
   ],
 }
