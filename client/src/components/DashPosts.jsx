691df5923b49466fc2eafdb014d7d7ae778d5c5b
Sachin Singh <sachinsingh9272@gmail.com>
2024-12-29 02:43:06 +0530

    Add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,20 +6,17 @@ import {Link} from 'react-router-dom'
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
-
+  const [showMore, setShowMore] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
-      console.log("Hiiiiqwe");
-
       try {
-        console.log("ouytrHiiii");
-
         const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
-        console.log("H");
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -30,8 +27,26 @@ const DashPosts = () => {
     }
   }, [currentUser?._id]);
 
+  const handleShowMore = async() => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev)=>[...prev, ...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
+
   return <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
     {currentUser.isAdmin && userPosts.length > 0 ? (
+      <>
       <Table hoverable className="shadow-md">
         <Table.Head>
           <Table.HeadCell>Date updated</Table.HeadCell>
@@ -74,10 +89,16 @@ const DashPosts = () => {
           </Table.Body>
         ))}
       </Table>
+      {showMore && (
+        <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">
+          Show more
+        </button>
+      )}
+      </>
     ):(
-      <p></p>
+      <p>You have no posts yet!</p>
     )}
-  </div>;
+  </div>
 };
 
 export default DashPosts;
