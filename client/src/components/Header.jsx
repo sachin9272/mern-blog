59980b9cb8e01987384bf32c0ec93341eeeae23c
Sachin Singh <sachinsingh9272@gmail.com>
2025-01-14 23:54:25 +0530

    Created search page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,51 +3,50 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
-import {useSelector,useDispatch} from 'react-redux';
-import {toggleTheme} from "../redux/theme/themeSlice";
-import {signoutSuccess} from "../redux/user/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const path = useLocation().pathname;
-    const location = useLocation();
-    const dispatch = useDispatch();
-    const {currentUser} = useSelector(state => state.user);
-    const {theme} = useSelector((state) => state.theme);
-    const [searchTerm, setSearchTerm] = useState('');
-    console.log(searchTerm);
-    
-    
-    useEffect(() => {
-      const urlParams = new URLSearchParams(location.search);
-      const searchTermFromUrl = urlParams.get('searchTerm');
-      if(searchTermFromUrl){
-        setSearchTerm(searchTermFromUrl);
+  const navigate = useNavigate();
+  const path = useLocation().pathname;
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
       }
-    }, [location.search])
-    
-    const handleSignout = async() => {
-          try {
-            const res = await fetch('/api/user/signout', {
-              method: 'POST',
-            });
-            const data = await res.json();
-            if(!res.ok){
-              console.log(data.message);
-            }else{
-              dispatch(signoutSuccess());
-            }
-          } catch (error) {
-            console.log(error.message);
-          }
-        }
-        const handleSubmit = (e) => {
-          e.preventDefault();
-          const urlParams = new URLSearchParams(location.search);
-          urlParams.set('searchTerm', searchTerm);
-          const searchQuery = urlParams.toString();
-          navigate(`/search/${searchQuery}`);
-        }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search/${searchQuery}`);
+  };
   return (
     <Navbar className="border-b-2">
       <Link
@@ -66,49 +65,59 @@ const Header = () => {
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
           value={searchTerm}
-          onChange={(e)=>setSearchTerm(e.target.value)}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
-          {
-            theme === 'light' ? <FaSun/> : <FaMoon/>
-          }
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
-          <Dropdown arrowIcon={false} inline label={
-            <Avatar alt="user" img={currentUser.profilePicture} rounded/>
-          }>
-          <Dropdown.Header>
-            <span className="block text-sm">@{currentUser.username}</span>
-            <span className="block text-sm font-medium truncate">{currentUser.email}</span>
-          </Dropdown.Header>
-          <Link to='/dashboard?tab=profile'>
-            <Dropdown.Item>Profile</Dropdown.Item>
-          </Link>
-          <Dropdown.Divider/>
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to="/dashboard?tab=profile">
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
             <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
-        ):
-        (
+        ) : (
           <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
-        </Link>
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
         )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
-          <Link to='/'>Home</Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/about'} as={'div'}>
-          <Link to='/about'>About</Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
